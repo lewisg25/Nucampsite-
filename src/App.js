1699a996a6ba2f69ad35fb934e7bb009f7064cad
Lewis G <lewis.garnett96@yahoo.com>
2024-01-05 22:42:07 -0500

    added formik

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const initialValues = {
+    username: '',
+    password: ''
+};
+
+const validateForm = () => ({});
+
+const handleSubmit = (values) => {
+    console.log(values);
+};
+
+const SimpleForm = () => {
+    return (
+        <Formik
+            initialValues={initialValues}
+            validate={validateForm}
+            onSubmit={handleSubmit}
+        >
+            <Form>
+                <Field type='username' name='username' />
+                <ErrorMessage name='username' />
+                <Field type='password' name='password' />
+                <ErrorMessage name='password' />
+                <button type='submit'>Login</button>
+            </Form>
+        </Formik>
+    );
+};
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='contact' element={<ContactPage />} />
-                <Route path='directory' element={<CampsitesDirectoryPage />} />
-            </Routes>
-            <Footer />
-        </div>
-    );
+    return <SimpleForm />;
 }
 
 export default App;
+// import { Routes, Route } from 'react-router-dom';
+// import ContactPage from './pages/ContactPage';
+// import HomePage from './pages/HomePage';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+// import './App.css';
+
+// function App() {
+//     return (
+//         <div className='App'>
+//             <Header />
+//             <Routes>
+//                 <Route path='/' element={<HomePage />} />
+//                 <Route path='contact' element={<ContactPage />} />
+//                 <Route path='directory' element={<CampsitesDirectoryPage />} />
+//             </Routes>
+//             <Footer />
+//         </div>
+//     );
+// }
+
+// export default App;
 
 // import { Routes, Route } from "react-router-dom";
 // import ContactPage from "./pages/ContactPage";
